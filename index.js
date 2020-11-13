cf60466c1ae3c073f73e8280650d97e2736e11f4
binding-event-8377 <marwa.al.shahal@gmail.com>
2020-11-13 20:35:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const pets=['Milo', 'Otis', 'Garfield'];
 pets.push ('Odie'); 
-
+const pets=['Milo', 'Otis', 'Garfield'];
 pets.unshift ('odie');
+const pets=['Milo', 'Otis', 'Garfield'];
 pets.pop();
+const pets=['Milo', 'Otis', 'Garfield'];
 pets.shift();
